9979cc7ebce691eefb93041f3b8c565a016d6a42
juandavidac <jd.ac@outlook.com>
2016-02-15 14:51:44 -0500

    Authorization on questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :set_question, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, except: [:index, :show]
 
   # GET /questions
   # GET /questions.json
@@ -69,6 +70,6 @@ class QuestionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def question_params
-      params.require(:question).permit(:title, :description)
+      params.require(:question).permit(:title, :description).merge(user: current_user)
     end
 end
