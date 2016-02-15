a62742873a17562690dc84354bb9ae43e085079b
juandavidac <jd.ac@outlook.com>
2016-02-15 14:36:23 -0500

    add association question user

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160215190758) do
+ActiveRecord::Schema.define(version: 20160215193023) do
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
+
+  add_index "questions", ["user_id"], name: "index_questions_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
