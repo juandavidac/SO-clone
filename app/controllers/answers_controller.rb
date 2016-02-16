5fa959bcc3b59755ab7afe2caacd6df75b6555a8
juandavidac <jd.ac@outlook.com>
2016-02-16 11:13:53 -0500

    agrega repsuestas y relaciona a usuarios

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,12 @@
+class AnswersController < ApplicationController
+  def create
+    question= Question.find(params[:question_id])
+    question.answers.create(answer_params)
+    redirect_to question
+  end
+
+private
+  def answer_params
+    params.require(:answer).permit(:body).merge(user: current_user)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160215193023) do
+ActiveRecord::Schema.define(version: 20160216160318) do
+
+  create_table "answers", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "question_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+  end
+
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id"
+  add_index "answers", ["user_id"], name: "index_answers_on_user_id"
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
